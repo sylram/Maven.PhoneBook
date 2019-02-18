99ecb92aa00112e3ff15bfb288217cc6ac362b6a
sylram <sylram7@gmail.com>
2019-02-17 21:03:33 -0500

    tests done

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,7 +1,142 @@
 package com.zipcodewilmington.phonebook;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.*;
+import java.lang.String;
+import java.util.Map;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+
+    Map<String, ArrayList> phoneRecords;
+    ArrayList<String> phoneList;
+
+    public PhoneBook() {
+        phoneRecords = new HashMap<String, ArrayList>();
+
+    }
+
+    //Map.Entry<String,ArrayList> byName = phoneRecords.entrySet();
+//    String nameEntry = byName.getValue();
+//    String phoneNumber = byName.getValue();
+
+
+    public void add(String name, String phone) {
+        phoneList = phoneRecords.get(name);
+        if (phoneList == null) {
+            phoneList = new ArrayList<String>();
+            phoneList.add(phone);
+            phoneRecords.put(name, phoneList);
+
+        } else {
+            phoneList.add(phone);
+        }
+
+
+    }
+
+    public ArrayList<String> get(String name) {
+        ArrayList<String> getPhones = phoneRecords.get(name);
+
+        return getPhones;
+
+    }
+
+    public void remove(String name) {
+        phoneRecords.remove(name);
+    }
+
+
+    public void removePhoneRecord(String name, String phone) {
+        phoneRecords.get(name);
+        phoneList.remove(phone);
+    }
+
+
+    public String reverseLookup(String givenPhone) {
+        String match = null;
+        for (String entry : phoneRecords.keySet()) {
+            if (phoneRecords.get(entry).contains(givenPhone)) {
+                match = entry;
+
+            }
+        }
+        return match;
+    }
+
 }
+
+//    public String display(ArrayList<String> n){
+//
+//
+//
+//        for (int i = 0; i <n.size() ; i++) {
+//            System.out.println(n.get(i).toString());
+//
+//        }
+//// IMPORTANT: Save the old System.out!
+//        PrintStream oldPrintStream = System.out;
+//// Tell Java to use your special stream
+//        System.setOut(printStream);
+//// Print some output: goes to your special stream
+//        printStream.print("this is"+ n);
+//// Put things back
+//        System.out.flush();
+//        System.setOut(oldPrintStream);}}
+//// Show what happened
+
+
+
+
+
+
+
+
+//
+//          }           key = entry.getKey();
+//break; //breaking because its one to one map
+
+//        String val = null;
+//        Set ref = phoneRecords.keySet();
+//        Iterator it = ref.iterator();
+//        List list = new ArrayList();
+//
+//        while (it.hasNext()) {
+//            Object o = it.next();
+//            if (phoneRecords.get(o).equals(givenPhone)) {
+//                list.add(o);
+//                val =list.toString();
+//
+
+
+
+//        String key=null;
+//        for(Map.Entry entry: phoneRecords.entrySet()){
+//
+//            if(givenPhone.equals(entry.getValue())){
+//                key = entry.getKey();
+//        }
+
+
+//        for (Map.Entry entry : phoneRecords.entrySet()) {
+//            if (givenPhone.equals(entry.getValue())) {
+//                key = entry.getKey();
+//                break; //breaking because its one to one map
+
+
+
+
+
+
+
+
+
+
+
+
+//ArrayList<String> phoneList = new ArrayList<String>();
+//        phoneList.add(phone);
+//        phoneRecords.put(name,phoneList);
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -1,7 +1,137 @@
 package com.zipcodewilmington.phonebook;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBookTest {
+
+    PhoneBook test;
+    String givenName;
+    String givenPhone;
+
+    @Before
+    public void trial() {
+        test = new PhoneBook();
+        givenName = "Rob Smith";
+        givenPhone = "4012568753";
+
+    }
+
+//TODO testAdd(String name, String phoneNumber)
+//adds an entry to the composite associate data type
+    @Test
+    public void testAddingRecord(){
+    //Given
+        test.add(givenName,givenPhone);
+
+    //When
+    ArrayList<String> retrieve = test.get(givenName);
+    String phoneNumber = retrieve.get(0);
+
+
+    //Then
+    Assert.assertEquals(givenPhone, phoneNumber);
+
 }
+//TODO testRemove(String name)
+//removes an entry to the composite associate data type
+    @Test
+    public void testRemove(){
+    //Given
+    test.add(givenName,givenPhone);
+
+    //When
+        test.remove(givenName);
+        ArrayList<String > retrieve = test.get(givenName);
+
+        //Then
+        Assert.assertNull(retrieve);
+
+    }
+
+//TODO testLookup(String name)
+//returns a phone number for the respective input name
+    @Test
+    public void testLookup(){
+    //Given
+    test.add(givenName,givenPhone);
+
+    //When
+    ArrayList<String> retrieve = test.get(givenName);
+    String phoneNumber = retrieve.get(0);
+
+    //Then
+    Assert.assertEquals(givenPhone,phoneNumber);
+
+}
+
+//TODO testReverseLookup(String phoneNumber)
+//returns a name for the respective input phoneNumber
+@Test
+    public void testReverseLookup() {
+    //Given
+    test.add(givenName,givenPhone);
+
+
+    //When
+    String retrieve = test.reverseLookup(givenPhone);
+
+
+    //Then
+    Assert.assertEquals(givenName, retrieve);
+
+}
+//TODO testDisplay()
+
+
+    @Test
+    public void testAddingSecondNumber() {
+        //Given
+        String num2 = "432957359272";
+        test.add(givenName,givenPhone);
+        test.add(givenName, num2);
+
+        //When
+        ArrayList<String> retrieve = test.get(givenName);
+        String phoneNumber = retrieve.get(1);
+
+        //Then
+        Assert.assertEquals(num2, phoneNumber);
+
+    }
+
+    @Test
+    public void removeOneNumber() {
+        //Given
+        String num2 = "432957359272";
+        test.add(givenName,givenPhone);
+        test.add(givenName, num2);
+        System.out.println(test.get(givenName).size());
+
+
+        //When
+        test.removePhoneRecord(givenName,givenPhone);
+        ArrayList<String> retrieve = test.get(givenName);
+        System.out.println(test.get(givenName));
+        String phoneNumber = retrieve.get(0);
+
+        //Then
+        Assert.assertEquals(num2, phoneNumber);
+
+    }
+
+
+
+
+
+
+
+    }
